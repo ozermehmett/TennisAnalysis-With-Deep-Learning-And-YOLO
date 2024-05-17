7cb86ceb758268fee9795f620cf651662a3fcfb5
ozermehmett <ozermehmet911@gmail.com>
2024-05-18 01:53:21 +0300

    edits and additions

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,4 @@
 from .video_utils import read_video, save_video
-from .box_utils import get_center_of_box, measure_distance
+from .box_utils import get_center_of_box, measure_distance, get_foot_position, get_closest_keypoint_index, get_height_of_bbox, measure_xy_distance, get_center_of_bbox
+from .conversions import convert_pixel_distance_to_meters, convert_meters_to_pixel_distance
+from .player_stats_drawer_utils import draw_player_stats
--- utils/box_utils.py
+++ utils/box_utils.py
@@ -7,3 +7,34 @@ def get_center_of_box(box):
 
 def measure_distance(p1,p2):
     return ((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)**0.5
+
+
+def get_foot_position(bbox):
+    x1, y1, x2, y2 = bbox
+    return (int((x1 + x2) / 2), y2)
+
+
+def get_closest_keypoint_index(point, key_points, keypoint_indices):
+    closest_distance = float('inf')
+    key_point_ind = keypoint_indices[0]
+    for keypoint_index in keypoint_indices:
+        keypoint = key_points[keypoint_index * 2], key_points[keypoint_index * 2 + 1]
+        distance = abs(point[1] - keypoint[1])
+
+        if distance < closest_distance:
+            closest_distance = distance
+            key_point_ind = keypoint_index
+
+    return key_point_ind
+
+
+def get_height_of_bbox(bbox):
+    return bbox[3]-bbox[1]
+
+
+def measure_xy_distance(p1,p2):
+    return abs(p1[0]-p2[0]), abs(p1[1]-p2[1])
+
+
+def get_center_of_bbox(bbox):
+    return (int((bbox[0]+bbox[2])/2),int((bbox[1]+bbox[3])/2))
